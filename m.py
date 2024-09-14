52e9702b533020313d64828197db75dc61516b9d
princeniki <107321705+princeniki@users.noreply.github.com>
2024-09-14 11:50:51 +0530

    Update m.py

--- m.py
+++ m.py
@@ -1,1 +1,24 @@
+from aiogram import Bot, Dispatcher, executor, types
+import os
+from keep_alive import keep_alive
+keep_alive()
+
+bot = Bot(token=os.environ.get('token'))
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start', 'help'])
+async def welcome(message: types.Message):
+    await message.reply("Hello! Im Gunther Bot, Please follow my YT channel")
+
+@dp.message_handler(commands=['logo'])
+async def logo(message: types.Message):
+    await message.answer_photo('https://avatars.githubusercontent.com/u/62240649?v=4')
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.reply(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
 
